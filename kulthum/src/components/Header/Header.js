1cdeef8bec2b33170078e365f9af6fc429f796db
KulthumRadi <kulthumradi94@gmail.com>
2022-06-13 22:01:52 +0300

    cleaning the code and creatibng the navigation

--- kulthum/src/components/Header/Header.js
+++ kulthum/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './style.css';
 export class Header extends Component {
   render() {
     return (
-      <header>Naya's Application using React that has Registration and Login Pages with Validation using Regex</header>
+      <header> Application using React that has Registration and Login Pages with Validation using Regex</header>
     )
   }
 }
